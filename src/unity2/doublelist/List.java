4a51572c516391291e2931fb02b30d44d9f3db70
brandon mutiz <>
2016-03-14 23:27:51 -0500

    "unity2.doublelist"

---
+++ src/unity2/doublelist/List.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unity2.doublelist;
+
+/**
+ *
+ * @author 
+ */
+public class List {
+    private Node head;
+ 
+   public List() {
+       head = null;
+    }
+
+   public void add(int d) {
+        Node n = new Node(d);
+        if (head == null) {
+           head = n;
+     } else {
+         Node aux = head;
+     while (aux.next != null) {
+             aux = aux.next;
+      }
+   aux.next = n;
+  n.back=aux;
+   }
+     }
+ 
+     public void print() {
+         Node aux = head;
+         while (aux != null) {
+            System.out.print(" " + aux.data);
+             aux = aux.next;
+         }
+        System.out.println();
+ 
+     }
+  public void ReversePrint(){
+         if (head==null)return;
+         Node aux = head;
+         while(aux.next != null){
+             aux = aux.next;
+         }
+         while ( aux.next!= null){
+            System.out.println(" "+aux.data);
+             aux=aux.back;
+         }
+     }
+     public void Push(int d){
+         Node n = new Node(d);
+         if(head==null){
+             head=n;
+         }else{
+             n.next=head;
+             head.back=n;
+             head=n;
+         }
+    
+ }
+     
+}
+
+
---
+++ src/unity2/doublelist/Test.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unity2.doublelist;
+
+/**
+ *
+ * @author 
+ */
+public class Test {
+    public static void main(String[] args){
+         
+         List l = new List();
+         l.add(2);
+         l.add(3);
+         l.add(8);
+         l.add(7);
+         l.add(5);
+                  System.out.print("initial values:");
+         l.print();  
+     }    
+
+}
