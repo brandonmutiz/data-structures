39ff57e5bf017a78ef9eb4c83ec58d8a782ea6d9
AULA1 <AULA1@192.168.31.1>
2016-04-12 17:17:15 -0500

    "Graph"

---
+++ src/unity1/grapher/Coordinate.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unity1.grapher;
+
+/**
+ *
+ * @author AULA1
+ */
+public class Coordinate {
+    public int x,y;
+    protected Coordinate next, back;
+    
+    public Coordinate(int x,int y){
+        this.x=x;
+        this.y=y;
+        next=back=null;
+    }
+}
